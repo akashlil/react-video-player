370d101fd64c6dee8e4be2f29c44ba19fd63dace
akashlil <akashlil393@gmail.com>
2024-02-05 00:08:24 +0600

    changes this video style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import VideoDetails from "./pages/VideoDetails";
 import Navbar from "./components/NavBar";
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/video/:id" element={<VideoDetails />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-import VideoCard from "../components/VideoCard";
+// VideoPlayer.jsx
 
+import React, { useRef, useEffect, useState } from "react";
+import Plyr from "plyr";
+import "plyr/dist/plyr.css"; // Import Plyr styles
 const videoList = [
   {
     id: 1,
@@ -57,40 +59,118 @@ const videoList = [
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9XMbR4wA5Os7Yig9OcBDagg5t9ZpZ7_H-XY0Jxr8SBQ&s",
   },
 ];
-export default function Home() {
-  return (
-    <div className="container py-5">
-      <VideoCard props={videoList} />
 
-      <nav aria-label="Page navigation example mt-5">
-        <ul class="pagination">
-          <li class="page-item">
-            <a class="page-link" href="#">
-              Previous
-            </a>
-          </li>
-          <li class="page-item">
-            <a class="page-link" href="#">
-              1
-            </a>
-          </li>
-          <li class="page-item">
-            <a class="page-link" href="#">
-              2
-            </a>
-          </li>
-          <li class="page-item">
-            <a class="page-link" href="#">
-              3
-            </a>
-          </li>
-          <li class="page-item">
-            <a class="page-link" href="#">
-              Next
-            </a>
-          </li>
-        </ul>
-      </nav>
+const VideoPlayer = () => {
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const [player, setPlayer] = useState(null);
+  const playerRef = useRef(null);
+
+  useEffect(() => {
+    if (playerRef.current) {
+      // Destroy the Plyr player if it exists
+      if (player) {
+        player.destroy();
+      }
+
+      // Create a new Plyr player
+      const newPlayer = new Plyr(playerRef.current, {
+        controls: [
+          "play-large",
+          "play",
+          "progress",
+          "current-time",
+          "mute",
+          "volume",
+          "settings",
+          "fullscreen",
+        ],
+        seekTime: 10,
+        volume: 1,
+        muted: false,
+        loop: { active: true },
+      });
+
+      // Set the Plyr player instance in the state
+      setPlayer(newPlayer);
+
+      // Change video when the component mounts
+      changeVideo(currentVideoIndex);
+    }
+  }, [currentVideoIndex]);
+
+  // Function to change the video source
+  const changeVideo = (index) => {
+    setCurrentVideoIndex(index);
+
+    if (playerRef.current && videoList[index]) {
+      playerRef.current.source = {
+        type: "video",
+        sources: [
+          {
+            src: videoList[index].src,
+            type: "video/mp4",
+          },
+        ],
+      };
+
+      // Reload the Plyr player
+      playerRef.current.load();
+    }
+  };
+
+  // Function to play the previous video
+  const playPrev = () => {
+    const prevIndex =
+      (currentVideoIndex - 1 + videoList.length) % videoList.length;
+    changeVideo(prevIndex);
+  };
+
+  // Function to play the next video
+  const playNext = () => {
+    const nextIndex = (currentVideoIndex + 1) % videoList.length;
+    changeVideo(nextIndex);
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-8">
+          <div className="video-player">
+            <video ref={playerRef} controls className="plyr">
+              <source src={videoList[currentVideoIndex].src} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            <div className="player-controls">
+              <button className="btn btn-light" onClick={playPrev}>
+                Previous
+              </button>
+              <button className="btn btn-light" onClick={playNext}>
+                Next
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="video-list">
+            <h2>Video List</h2>
+            <ul className="list-group">
+              {videoList.map((video, index) => (
+                <li
+                  key={index}
+                  onClick={() => changeVideo(index)}
+                  className={`list-group-item ${
+                    index === currentVideoIndex ? "active" : ""
+                  }`}
+                >
+                  Video {index + 1}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default VideoPlayer;
