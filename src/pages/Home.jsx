c82c9f0f5d786b3f47eb6ab795ee26feeb927354
akashlil <akashlil393@gmail.com>
2024-02-04 23:01:13 +0600

    sklef

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const videoList = [
   {
     id: 1,
     title: "Suipoka live show ||pilak mela||band suipoka || somu &",
-    src: "../../public/video_1.mp4",
+    src: "https://ak.storerepublic.com/public/video_1.mp4",
     details:
       "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
     thumbnail:
@@ -14,7 +14,7 @@ const videoList = [
   {
     id: 2,
     title: "আলাদা আলাদা male version by Anupam Roy. আমি আবার ক্লান্ত",
-    src: "../../public/video_2.mp4",
+    src: "https://ak.storerepublic.com/public/video_2.mp4",
     details:
       "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
     thumbnail:
@@ -23,7 +23,7 @@ const videoList = [
   {
     id: 3,
     title: "( Lofi Box ) One Hours Bengali Emotional Lofi Remix Song |",
-    src: "../../public/video_3.mp4",
+    src: "https://ak.storerepublic.com/public/video_3.mp4",
     details:
       "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
     thumbnail:
@@ -32,7 +32,7 @@ const videoList = [
   {
     id: 4,
     title: "Suipoka live show ||pilak mela||band suipoka || somu &",
-    src: "../../public/video_1.mp4",
+    src: "https://ak.storerepublic.com/public/video_1.mp4",
     details:
       "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
     thumbnail:
@@ -41,7 +41,7 @@ const videoList = [
   {
     id: 5,
     title: "আলাদা আলাদা male version by Anupam Roy. আমি আবার ক্লান্ত",
-    src: "../../public/video_2.mp4",
+    src: "https://ak.storerepublic.com/public/video_2.mp4",
     details:
       "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
     thumbnail:
@@ -50,34 +50,7 @@ const videoList = [
   {
     id: 6,
     title: "( Lofi Box ) One Hours Bengali Emotional Lofi Remix Song |",
-    src: "../../public/video_3.mp4",
-    details:
-      "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
-    thumbnail:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9XMbR4wA5Os7Yig9OcBDagg5t9ZpZ7_H-XY0Jxr8SBQ&s",
-  },
-  {
-    id: 7,
-    title: "Suipoka live show ||pilak mela||band suipoka || somu &",
-    src: "../../public/video_1.mp4",
-    details:
-      "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
-    thumbnail:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9XMbR4wA5Os7Yig9OcBDagg5t9ZpZ7_H-XY0Jxr8SBQ&s",
-  },
-  {
-    id: 8,
-    title: "আলাদা আলাদা male version by Anupam Roy. আমি আবার ক্লান্ত",
-    src: "../../public/video_2.mp4",
-    details:
-      "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
-    thumbnail:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9XMbR4wA5Os7Yig9OcBDagg5t9ZpZ7_H-XY0Jxr8SBQ&s",
-  },
-  {
-    id: 9,
-    title: "( Lofi Box ) One Hours Bengali Emotional Lofi Remix Song |",
-    src: "../../public/video_3.mp4",
+    src: "https://ak.storerepublic.com/public/video_3.mp4",
     details:
       "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
     thumbnail:
@@ -88,6 +61,36 @@ export default function Home() {
   return (
     <div className="container py-5">
       <VideoCard props={videoList} />
+
+      <nav aria-label="Page navigation example mt-5">
+        <ul class="pagination">
+          <li class="page-item">
+            <a class="page-link" href="#">
+              Previous
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              1
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              2
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              3
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
--- src/pages/VideoDetails.jsx
+++ src/pages/VideoDetails.jsx
@@ -8,7 +8,7 @@ const videoList = [
   {
     id: 1,
     title: "Suipoka live show ||pilak mela||band suipoka || somu &",
-    src: "../../public/video_1.mp4",
+    src: "https://ak.storerepublic.com/public/video_1.mp4",
     details:
       "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
     thumbnail:
@@ -17,7 +17,7 @@ const videoList = [
   {
     id: 2,
     title: "আলাদা আলাদা male version by Anupam Roy. আমি আবার ক্লান্ত",
-    src: "../../public/video_2.mp4",
+    src: "https://ak.storerepublic.com/public/video_2.mp4",
     details:
       "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
     thumbnail:
@@ -26,7 +26,7 @@ const videoList = [
   {
     id: 3,
     title: "( Lofi Box ) One Hours Bengali Emotional Lofi Remix Song |",
-    src: "../../public/video_3.mp4",
+    src: "https://ak.storerepublic.com/public/video_3.mp4",
     details:
       "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
     thumbnail:
@@ -35,7 +35,7 @@ const videoList = [
   {
     id: 4,
     title: "Suipoka live show ||pilak mela||band suipoka || somu &",
-    src: "../../public/video_1.mp4",
+    src: "https://ak.storerepublic.com/public/video_1.mp4",
     details:
       "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
     thumbnail:
@@ -44,7 +44,7 @@ const videoList = [
   {
     id: 5,
     title: "আলাদা আলাদা male version by Anupam Roy. আমি আবার ক্লান্ত",
-    src: "../../public/video_2.mp4",
+    src: "https://ak.storerepublic.com/public/video_2.mp4",
     details:
       "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
     thumbnail:
@@ -53,40 +53,14 @@ const videoList = [
   {
     id: 6,
     title: "( Lofi Box ) One Hours Bengali Emotional Lofi Remix Song |",
-    src: "../../public/video_3.mp4",
-    details:
-      "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
-    thumbnail:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9XMbR4wA5Os7Yig9OcBDagg5t9ZpZ7_H-XY0Jxr8SBQ&s",
-  },
-  {
-    id: 7,
-    title: "Suipoka live show ||pilak mela||band suipoka || somu &",
-    src: "../../public/video_1.mp4",
-    details:
-      "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
-    thumbnail:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9XMbR4wA5Os7Yig9OcBDagg5t9ZpZ7_H-XY0Jxr8SBQ&s",
-  },
-  {
-    id: 8,
-    title: "আলাদা আলাদা male version by Anupam Roy. আমি আবার ক্লান্ত",
-    src: "../../public/video_2.mp4",
-    details:
-      "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
-    thumbnail:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9XMbR4wA5Os7Yig9OcBDagg5t9ZpZ7_H-XY0Jxr8SBQ&s",
-  },
-  {
-    id: 9,
-    title: "( Lofi Box ) One Hours Bengali Emotional Lofi Remix Song |",
-    src: "../../public/video_3.mp4",
+    src: "https://ak.storerepublic.com/public/video_3.mp4",
     details:
       "7,792,761 views  Premiered on 8 Jun 2023  James Lyrics Song 2023 In this video presenting. James new bangla song 2023. I hope you like the song a lot. So enjoy the song Song : কিসের এত দুঃখ তোমার সারাক্ষণ বসে বসে ভাবছো  |",
     thumbnail:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9XMbR4wA5Os7Yig9OcBDagg5t9ZpZ7_H-XY0Jxr8SBQ&s",
   },
 ];
+
 export default function () {
   const { id } = useParams();
   const videoDetails = videoList.find(
